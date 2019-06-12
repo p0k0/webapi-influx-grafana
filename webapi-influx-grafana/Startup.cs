5f12c6392215721f876600cb8001843cc020fc31
bulatm <m11e@yandex.ru>
2019-06-12 15:14:07 +0300

    web api with metrics tracking in influxdb and grafana

---
+++ webapi-influx-grafana/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Metrics.Configuration;
+using App.Metrics.Extensions.Reporting.InfluxDB;
+using App.Metrics.Extensions.Reporting.InfluxDB.Client;
+using App.Metrics.Reporting.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace webapi_influx_grafana
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var database = "appmetricsdemo";
+            var uri = new Uri("http://127.0.0.1:8086");
+
+            services.AddMetrics(options =>
+            {
+                options.WithGlobalTags((globalTags, info) =>
+                {
+                    globalTags.Add(" app ", info.EntryAssemblyName);
+                    globalTags.Add(" env ", " stage ");
+                });
+            })
+                .AddHealthChecks()
+                .AddReporting(
+                    factory =>
+                    {
+                        factory.AddInfluxDb(
+                            new InfluxDBReporterSettings
+                            {
+                                InfluxDbSettings = new InfluxDBSettings(database, uri),
+                                ReportInterval = TimeSpan.FromSeconds(5)
+                            });
+                    })
+                .AddMetricsMiddleware(options => options.IgnoredHttpStatusCodes = new[] { 404 });
+            services.AddMvc(options => options.AddMetricsResourceFilter());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IApplicationLifetime lifetime)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection(" Logging "));
+            app.UseMetrics();
+            app.UseMetricsReporting(lifetime);
+            app.UseMvc();
+        }
+    }
+}
